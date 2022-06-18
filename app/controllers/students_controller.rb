42ab281b7d4a561c849b1266e4422fa56caf3a99
Albert Byrone <albertbyrone1677@gmail.com>
2022-06-18 08:44:07 +0300

    commit tree 6382ef20fc300f49df584ac452b7338a4684a50d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    puts "this it the param: #{params[:name]}"
+    students =  if params[:name]
+                  name = params[:name]
+                  Student.where('first_name LIKE ? OR last_name LIKE ?', "%#{name}%", "%#{name}%")
+                else
+                  Student.all
+                end
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 
+
+  # def self.search(name)
+  #   Student.where('first_name LIKE ? OR last_name LIKE ?', "%#{name}%", "%#{name}%")
+  # end
 end
